242810cd0fbf8a35bbb71f4b73d9a15b4805b477
joshhchun <jchun2@nd.edu>
2024-04-23 13:22:33 -0400

    Functionalities of backend: 1. Turn user inputted images to URLs using Pym 2. Get list of ingredients from turbo-4 (GPT) from image urls 3. Get list of recipes in an array of JSON objects with ingredients (using turbo 3.5, only need text + probably cheaper)
    TODO:
    1. Hardcoding preferences right now, need user page to do that organically
    2. Multiple image urls

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -38,28 +38,55 @@ async function uploadToPym(files: Files<string>) {
     return imageUrls
 }
 
+// TODO: Handle multiple images
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
     try {
         const form = formidable({ multiples: true })
         const [fields, files] = await form.parse(req)
+
+        // Turn images into links using https://pym.jchun.me
         const imageUrls: string[] | undefined = await uploadToPym(files);
-        console.log("image urls: ", imageUrls);
-        const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo-0125",
-            max_tokens: 4096,
+
+        // Get the ingredients from GPT by sending picture links
+        const ingredientsResponse = await openai.chat.completions.create({
+            model: "gpt-4-turbo",
             messages: [
                 {
                     role: "user",
                     content: [
-                        { type: "text", text: "What’s food ingredients are in this image?" },
+                        { type: "text", text: "Please list the ingredients in this picture separated by commas and with no descriptions." },
+                        {
+                            type: "image_url",
+                            image_url: {
+                                "url": `https://pym.jchun.me/api/${imageUrls[0]}`,
+                            },
+                        }
+                    ],
+                },
+            ],
+        });
+        const ingredients: string | null = ingredientsResponse.choices[0]["message"]["content"];
+        console.log("ingredients: ", ingredients)
+
+        // TODO: Hardcoding preferences for now, need a user page so we can acccess this
+        const preferences: string = "Prefer Italian and Asian cuisine. Allergic to eggs. Want cook time to be an hour or below."
+        // Getting recipes from GPT with ingredients retrieved from turbo-4
+        const recipeResponse = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "user",
+                    content: [
+                        { type: "text", text: `With these following ingredients, list possible recipes I can make. Include step by step directions, cooking time, cooking materials needed, serving portions, and ingredients needed: ${ingredients}. Make sure to take into account the following preferences: ${preferences}. Output the different recipes in an array format of JSON objects. For example, "[{'recipe_name': ..., 'cooking_time': ..., 'directions': ...}, {'recipe_name: ...}]"` },
                     ],
                 },
             ],
         });
 
-        const responseText: string = JSON.stringify(response.choices);
-        console.log("openAI response: ", responseText)
-        res.status(200).json({ result: responseText });
+        // Should be an array of JSON objects containing recipes
+        const recipes: string | null = recipeResponse.choices[0]["message"]["content"];
+        console.log("recipes: ", recipes)
+        res.status(200).json({ result: ingredients });
     } catch (error) {
         res.status(500).json({ error: "error" });
         console.error(error);
